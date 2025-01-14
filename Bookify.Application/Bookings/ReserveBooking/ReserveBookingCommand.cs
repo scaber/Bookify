376c799978d0f2d6e80fbc3608631bcfce48c2f0
Tayfun Kilic (Dogus Teknoloji) <Tayfun.Kilic@d-teknoloji.com.tr>
2025-01-15 00:30:42 +0300

    end 28 adde applcation layer and cqrs implemendting

---
+++ Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommand.cs
@@ -0,0 +1,18 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Bookify.Application.Bookings.ReserveBooking
+{
+    public record ReserveBookingCommand(
+        Guid ApartmentId,
+        Guid UserId,
+        DateOnly StartDate,
+        DateOnly EndDate
+        ) :ICommand<Guid>;
+    
+}
