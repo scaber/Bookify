ee68e0ca731ab048264401d369efad0991f0b0d6
Tayfun Kilic (Dogus Teknoloji) <Tayfun.Kilic@d-teknoloji.com.tr>
2025-01-17 10:34:52 +0300

    authentication end

---
+++ Bookify.Application/Abstractions/Email/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Abstractions.Email
+{
+    internal interface IEmailService
+    {
+        Task SendAsync(Domain.Users.Email recipient, string subject, string body);
+    }
+}
---
+++ Bookify.Application/Bookings/ReserveBooking/BookingReservedDomainEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using Bookify.Application.Abstractions.Email;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Bookings.Events;
+using Bookify.Domain.Users;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Bookings.ReserveBooking
+{
+    internal sealed class BookingReservedDomainEventHandler : INotificationHandler<BookingReservedDomainEvent>
+    {
+        private readonly IBookingRepository _bookingRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IEmailService _emailService;
+
+
+        public Task Handle(BookingReservedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
